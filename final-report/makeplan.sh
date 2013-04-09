200e2affd0e95f92f94be71b43f9cd468f06495c
Emanuil Evgeniev Tolev <emanuil.tolev@gmail.com>
2013-04-09 21:41:54 +0100

    more progress on final writeup

--- final-report/makeplan.sh
+++ final-report/makeplan.sh
@@ -1,13 +1,13 @@
 regex='(\\(sub){0,2}section|\\chapter)'
 out='plan.tex'
 cat Chapter1/chapter1.tex | egrep $regex > $out
-echo -e "\n\n" >> $out
+#echo -e "\n\n" >> $out
 cat Chapter2/chapter2.tex | egrep $regex >> $out
-echo -e "\n\n" >> $out
+#echo -e "\n\n" >> $out
 cat Chapter3/chapter3.tex | egrep $regex >> $out
-echo -e "\n\n" >> $out
+#echo -e "\n\n" >> $out
 cat Chapter4/chapter4.tex | egrep $regex >> $out
-echo -e "\n\n" >> $out
+#echo -e "\n\n" >> $out
 cat Chapter5/chapter5.tex | egrep $regex >> $out
-echo -e "\n\n" >> $out
+#echo -e "\n\n" >> $out
 cat Chapter6/chapter6.tex | egrep $regex >> $out
