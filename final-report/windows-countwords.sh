e05e22fd8e5024e7e61d8033cf0322f675e88a62
Emanuil Tolev <emanuil.tolev@gmail.com>
2013-04-24 17:46:23 +0100

    final report completed

--- final-report/windows-countwords.sh
+++ final-report/windows-countwords.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if (( $# != 1 ))
 then
-	perl /g/home/emanuil/software/TeXcount_3_0_beta/texcount.pl -dir -merge -sub=chapter wordcount.tex
+	perl /j/home/emanuil/software/TeXcount_3_0_beta/texcount.pl -dir -merge -sub=chapter wordcount.tex
 else
-	perl /g/home/emanuil/software/TeXcount_3_0_beta/texcount.pl -dir -merge "$@" wordcount.tex
+	perl /j/home/emanuil/software/TeXcount_3_0_beta/texcount.pl -dir -merge "$@" wordcount.tex
 fi
